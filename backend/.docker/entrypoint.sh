913c2cd4182b6ce4d3553d139750de2987c64dad
GustaVasconcelos <gustavo.vasconcelos.peixoto18@gmail.com>
2025-03-19 21:34:39 -0300

    feat: remove migrate do entrypoint

--- backend/.docker/entrypoint.sh
+++ backend/.docker/entrypoint.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
+
+composer install
+
+sleep 3
 if [ ! -f .env ]; then
     echo "Arquivo .env não encontrado. Copiando .env.example para .env"
     cp .env.example .env
 fi
 
-composer install 
-
-sleep 3
+php artisan jwt:secret
 
 php artisan key:generate
 
-php artisan jwt:secret
-
-php artisan migrate
-
-php artisan serve --host=0.0.0.0 --port=8000
+exec "$@"
